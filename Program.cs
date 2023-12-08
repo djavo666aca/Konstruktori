1a81971db0f9339f090f63e9ba2c2ebbd6e1ae21
djavo666aca <52077531+djavo666aca@users.noreply.github.com>
2023-12-08 19:23:55 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Konstruktori
+{
+    internal class Program
+    {
+        string status;
+
+        Program()
+        {
+            status = "Aktivan";
+        }
+        static void Main(string[] args)
+        {
+            Program p1 = new Program();
+            Console.WriteLine(p1.status);
+
+            Program p2 = new Program();
+            Console.WriteLine(p2.status);
+
+            p2.status = "Iskljuceno";
+            Console.WriteLine(p2.status);
+        }
+    }
+}
